6bff7b33670dcf69659d16d4dc0fdd62f88462a2
Amanskywalker <ak47su30ac@gmail.com>
2016-08-15 22:23:13 +0530

    controller to view marks  	modified:   app/Http/Controllers/MarksController.php

--- app/Http/Controllers/MarksController.php
+++ app/Http/Controllers/MarksController.php
@@ -65,4 +65,12 @@ class MarksController extends Controller
         $sub=array('x14mat11','x14phy12','x14civ13','x14eme14','x14ele15','x14wsl16','x14phyl17','x14cip18');
         return View('marks',['sub'=>$sub]);
     }
+
+    public function DisplayMarks()
+    {
+        $user=Auth::user();
+        $sub_sem_1=array('x14mat11','x14phy12','x14civ13','x14eme14','x14ele15','x14wsl16','x14phyl17','x14cip18');
+        $marks_1_1=DB::table('marks_1_1')->where('sid', '=', $user->id)-first();
+        return View('showmarks',['sub'=>$sub_sem_1,'marks_1_1'=>$marks_1_1]);
+    }
 }
