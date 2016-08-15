962aeb9725cc0eede8319869c1e317faf1352f8b
Amanskywalker <ak47su30ac@gmail.com>
2016-08-15 13:53:51 +0530

    modified:   resources/views/home.blade.php

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -8,7 +8,36 @@
                 <div class="panel-heading">Dashboard</div>
 
                 <div class="panel-body">
-                    You are logged in!
+                    You are logged in! <br>
+                    This is your dashboard
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+<!--Events feeds goes here-->
+<div class="container">
+    <div class="row">
+        <div class="col-md-10 col-md-offset-1">
+            <div class="panel panel-default">
+                <div class="panel-heading">Events</div>
+
+                <div class="panel-body">
+                    Events and notices feeds goes here
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+<!--Events feeds goes here-->
+<div class="container">
+    <div class="row">
+        <div class="col-md-10 col-md-offset-1">
+            <div class="panel panel-default">
+                <div class="panel-heading">Notice</div>
+
+                <div class="panel-body">
+                    More features comming soon.....
                 </div>
             </div>
         </div>
