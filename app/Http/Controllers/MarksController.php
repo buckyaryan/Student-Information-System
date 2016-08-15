69515873272c14fe90f77b613ab8a188a7e84bb0
Amanskywalker <ak47su30ac@gmail.com>
2016-08-15 20:00:50 +0530

    modified:   app/Http/Controllers/MarksController.php

--- app/Http/Controllers/MarksController.php
+++ app/Http/Controllers/MarksController.php
@@ -2,9 +2,21 @@
 
 namespace App\Http\Controllers;
 
-use Illuminate\Http\Request;
+use App\Student;                        // to use the user class
 
+use Validator;                          // to use the Validator functions to validate the user input
+
+use Illuminate\Http\Request;
+                                        // to use the request function
 use App\Http\Requests;
+
+use Illuminate\Support\Facades\Auth;    // to use predefined auth process
+
+use App\Http\Controllers\Controller;    // to use the Controller class
+
+use DB;                                 // to acess to the database
+
+use App\Http\Controllers\View;          // to acess the views functions
 
 class MarksController extends Controller
 {
